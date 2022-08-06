dca2ec2951cca5cff072da319d098df6b36b92c4
limon445 <53107717+limon445@users.noreply.github.com>
2022-08-06 21:04:02 +0600

    Add files via upload

---
+++ php/verification.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+  <link rel="stylesheet" href="..\style.css">
+<head>
+<body>
+  <div class="wrapper">
+    <section class="form login">
+      <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+        <div class="error-text"></div>
+        <div class="field input">
+          <label>Verify Code</label>
+          <input type="text" name="Verification_code" placeholder="Enter verification code" required>
+        </div>
+        <div class="field button">
+          <input type="submit" name="verify_email" value="Verify Email">
+        </div>
+      </form>
+     
+    </section>
+  </div>
+  
+  
+
+</body>
+</html>
